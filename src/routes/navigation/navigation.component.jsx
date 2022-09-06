fc6b3df55e76076db304f688a08ac7f442b64677
Jeyson U. Gonzalez <jeysongonzalezlopez@gmail.com>
2022-09-06 15:13:26 +0200

    adding firebase configuration

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";// react is usefull if you dont want to render some of the html elemnet
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrwnLogo} from "../../assets/crown.svg";
+import "./navigation-styles.scss"
+// Link is use to proper laberage(etiquetar) the proper url. read by the browsers. behaves like anchor tag <a>
+const Navigation = () => {
+    return(
+      <Fragment>
+        <div className="navigation">
+            <Link className='logo-container' to='/'>
+                <CrwnLogo className='logo' />
+            </Link>
+            <div className='nav-links-container'>
+            <Link className='nav-link' to='/shop'>
+                SHOP
+            </Link>
+            <Link className='nav-link' to='/sign-in'>
+                SIGN IN
+            </Link>
+            </div>
+          <div></div>
+        </div>
+        <Outlet />
+      </Fragment>
+    );
+  };
+
+  export default Navigation;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,62 @@
+import { initializeApp } from "firebase/app"; // initializeApp create the instance for based on configuration
+import { getAuth, 
+         signInWithRedirect, 
+         signInWithPopup, 
+         GoogleAuthProvider } from "firebase/auth";  //authentication package come with firebase package
+import {
+        getFirestore,
+        doc, 
+        getDoc,
+        setDoc } from "firebase/firestore";
+        //import some methods, getFirestore to instatiate
+        //doc to retrieve the docs instance
+        //getDoc to access the data
+        //to set the the data is setDoc
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABpgwUmMtSSecZtmGn6EdRJE3dUnWIavc",
+    authDomain: "crwn-clothing-v2-b8d48.firebaseapp.com",
+    projectId: "crwn-clothing-v2-b8d48",
+    storageBucket: "crwn-clothing-v2-b8d48.appspot.com",
+    messagingSenderId: "702774082872",
+    appId: "1:702774082872:web:24f8a4354ecd80442315cd",
+    measurementId: "G-1N0GV1H6T8"
+  };
+
+
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();//class we get from google authentication(auth)
+  provider.setCustomParameters({ //multiple provider are possible due to kind of sigin(popup/signInWithRedirect)
+    prompt: "select_account",
+  });
+
+export const auth = getAuth(); // it should be the same auth for any website/app
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);// doc takes 3 argument(database, collection,identifier(id))
+    const userSnapshot = await getDoc(userDocRef);
+    //if data exits
+    //return userDochref
+    //if does not exists, we need to create
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try{
+            await setDoc(userDocRef, {
+                displayName, email, createdAt
+            })
+        }catch(err){
+            console.log('error creating the user', err.message);
+        }
+    }
+
+    return userDocRef;
+
+};
